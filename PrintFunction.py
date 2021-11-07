d9edd2f288c808944686519e75963a530fdc2321
Susmita-Dey <79099734+Susmita-Dey@users.noreply.github.com>
2021-11-07 20:35:09 +0530

    Create PrintFunction.py

---
+++ PrintFunction.py
@@ -0,0 +1,9 @@
+# Question Link:- https://www.hackerrank.com/challenges/python-print
+
+if __name__ == '__main__':
+    n = int(input())
+    
+i = 1
+while i<=n:
+    print(i,end="")
+    i = i + 1
