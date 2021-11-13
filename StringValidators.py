1dba185a4eddac82b1c223afaece30df7f20d561
Susmita-Dey <79099734+Susmita-Dey@users.noreply.github.com>
2021-11-13 13:04:25 +0530

    Create StringValidators.py

---
+++ StringValidators.py
@@ -0,0 +1,27 @@
+#Wrong Code
+if __name__ == '__main__':
+    s = raw_input()
+    if s.isalnum():
+        print("True")
+    else:
+        print("False")
+    
+    if s.isalpha():
+        print("True")
+    else:
+        print("False")
+        
+    if s.isdigit():
+        print("True")
+    else:
+        print("False")
+        
+    if s.islower():
+        print("True")
+    else:
+        print("False")
+        
+    if s.isupper():
+        print("True")
+    else:
+        print("False")
