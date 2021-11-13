de8ac6f8ebfd183e5c1cba03628c3bff9fd3409e
Susmita-Dey <79099734+Susmita-Dey@users.noreply.github.com>
2021-11-13 12:54:45 +0530

    Create FindAString.py

---
+++ FindAString.py
@@ -0,0 +1,15 @@
+# Question Link:- https://www.hackerrank.com/challenges/find-a-string/problem #
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(0,len(string)):
+        if string[i:].startswith(sub_string):
+            count=count+1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
