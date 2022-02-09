4d3799e9d7f3aa73c4da9621b57c3687ca8bbfeb
Soumya Shankar Banerjee <35263147+banerjeesoumya15@users.noreply.github.com>
2022-02-09 16:07:48 +0530

    code fix
    fixed the solution code. output matches with expected output in hackerrank

--- StringValidators/StringValidators.py
+++ StringValidators/StringValidators.py
@@ -1,59 +1,35 @@
-#Wrong Code
-# if __name__ == '__main__':
-#     s = raw_input()
-#     if s.isalnum():
-#         print("True")
-#     else:
-#         print("False")
-    
-#     if s.isalpha():
-#         print("True")
-#     else:
-#         print("False")
-        
-#     if s.isdigit():
-#         print("True")
-#     else:
-#         print("False")
-        
-#     if s.islower():
-#         print("True")
-#     else:
-#         print("False")
-        
-#     if s.isupper():
-#         print("True")
-#     else:
-#         print("False")
-
-# Another Solution Still Wrong #
 if __name__ == '__main__':
     s = input()
     for c in s:
         val = False
         if c.isalnum():
             val=True
+            break
     print(val)
     for c in s:
         val = False
         if c.isalpha():
             val=True
+            break
     print(val)
         
     for c in s:
         val = False
         if c.isdigit():
             val=True
+            break
     print(val)
         
     for c in s:
         val = False
         if c.islower():
             val=True
+            break
     print(val)
         
     for c in s:
         val = False
         if c.isupper():
             val=True
+            break
     print(val)
